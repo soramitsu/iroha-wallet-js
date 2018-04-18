fb27e8caf215a94669737d511ffaa0bb2c243c3a
oimou <s1190013@gmail.com>
2018-04-18 16:47:57 +0900

    Add form validation functionality

--- src/renderer/components/Login.vue
+++ src/renderer/components/Login.vue
@@ -2,24 +2,32 @@
   <el-card class="login-form-container">
     <img id="logo" class="logo" src="~@/assets/logo.svg" alt="Iroha">
 
-    <el-form :model="form">
-      <el-form-item label="username:">
-        <el-input v-model="form.username"></el-input>
+    <el-form ref="form" :model="form" :rules="rules" label-position="top">
+      <el-form-item label="username:" prop="username">
+        <el-input name="username" v-model="form.username"></el-input>
       </el-form-item>
 
-      <el-form-item label="private key:">
-        <el-upload
-          ref="upload"
-          action=""
-          :auto-upload="false"
-          :file-list="form.fileList"
-        >
-          <el-button type="primary">choose</el-button>
-        </el-upload>
+      <el-form-item label="private key:" prop="privateKey">
+        <el-row type="flex" justify="space-between">
+          <el-col :span="17">
+            <el-input name="privateKey" v-model="form.privateKey"></el-input>
+          </el-col>
+
+          <el-col :span="6">
+            <el-upload
+              action=""
+              :auto-upload="false"
+              :show-file-list="false"
+              :on-change="onFileChosen"
+            >
+              <el-button type="primary">choose</el-button>
+            </el-upload>
+          </el-col>
+        </el-row>
       </el-form-item>
 
-      <el-form-item label="node ip:">
-        <el-input v-model="form.username"></el-input>
+      <el-form-item label="node ip:" prop="nodeIp">
+        <el-input v-model="form.nodeIp"></el-input>
       </el-form-item>
 
       <el-form-item>
@@ -32,13 +40,39 @@
 <script>
   export default {
     name: 'login',
+
     data () {
       return {
         form: {
           username: '',
           privateKey: '',
           nodeIp: ''
+        },
+        rules: {
+          username: [
+            { required: true, message: 'Please input username', trigger: 'change' },
+            { pattern: /^[a-z_0-9]{1,32}@[a-z_0-9]{1,9}$/, message: 'Username should match [a-Z_0-9]{1,32}@[a-Z_0-9]{1,9}', trigger: 'change' }
+          ],
+          privateKey: [
+            { required: true, message: 'Please input private key', trigger: 'change' },
+            { pattern: /^[A-Za-z0-9]{64}$/, message: 'Private key should match [A-Za-z0-9]{64}', trigger: 'change' }
+          ],
+          nodeIp: [
+            { required: true, message: 'Please input node ip', trigger: 'change' }
+          ]
         }
+      }
+    },
+
+    methods: {
+      onFileChosen (file, fileList) {
+        const reader = new FileReader()
+
+        reader.onload = (ev) => {
+          this.form.privateKey = (ev.target.result || '').trim()
+          this.$refs['form'].validate((valid) => console.log(valid))
+        }
+        reader.readAsText(file.raw)
       }
     }
   }
