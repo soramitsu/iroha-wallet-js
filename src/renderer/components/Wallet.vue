371e7696f223b4ad02d563649ef44cfb1039d908
Keiji Yahata <yahata@soramitsu.co.jp>
2018-04-24 15:04:59 +0900

    Make each wallet page obtain wallet data by the passed walletId

--- src/renderer/components/Wallet.vue
+++ src/renderer/components/Wallet.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wallet">
-    {{ walletId }}
+    {{ wallet.id }}
   </div>
 </template>
 
@@ -10,13 +10,32 @@
 
     data () {
       return {
-        walletId: this.$route.params.walletId
+        wallet: {}
       }
     },
 
     watch: {
       '$route' (to, from) {
-        this.walletId = to.params.walletId
+        this.fetchWalletByWalletId(this.$route.params.walletId)
+      }
+    },
+
+    created () {
+      this.fetchWalletByWalletId(this.$route.params.walletId)
+    },
+
+    methods: {
+      /**
+       * TODO: This is dummy and wallets master data are hardcoded for now.
+       * I guess it will be replaced with API call to fetch a wallet by walletId
+       * and will perform dispatching an action to a store or the like.
+       */
+      fetchWalletByWalletId (walletId) {
+        this.wallet = [
+          { id: 'wallet-1', name: 'dollar#russia', amount: '100.00' },
+          { id: 'wallet-2', name: 'yen#russia', amount: '100.00' },
+          { id: 'wallet-3', name: 'euro#russia', amount: '100.00' }
+        ].find((w) => (w.id === walletId))
       }
     }
   }
